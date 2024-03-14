beb81800f95188e7e1aee8468dcf389d7bc8c88e
Sam Leonard <samuelleonard35@gmail.com>
2024-03-14 01:12:46 +0000

    ✨ Fixed up examples...

--- path_traversal/safe.c
+++ path_traversal/safe.c
@@ -17,16 +17,16 @@ int main(int argc, char *argv[]) {
   if (asprintf(&web_root, "%s/web_root", cwd) < 0)
     die("Failed to get format web root: %m\n");
 
-  web_root_dirfd = open(web_root, O_DIRECTORY|O_RDONLY|O_CLOEXEC);
+  web_root_dirfd = open(web_root, O_DIRECTORY|O_RDONLY);
   if (web_root_dirfd < 0)
     die("Failed to open web root directory: %m\n");
 
-  struct open_how safe_open_how = { .flags = O_RDONLY|O_CLOEXEC, .resolve = RESOLVE_IN_ROOT };
-  file_fd = openat2(web_root_dirfd, argv[1], &safe_open_how, sizeof(struct open_how));
+  struct open_how safe_open_how = { .flags = O_RDONLY, .resolve = RESOLVE_IN_ROOT };
+  file_fd = (int) openat2(web_root_dirfd, argv[1], &safe_open_how, sizeof(struct open_how));
   if (file_fd < 0)
-    die("Failed to open %s: %m\n", argv[1]);
+    die("Failed to open %s: %m %d\n", argv[1], errno);
 
-  r = cat_file_fd(file_fd);
+  r = cat_file_inner(file_fd);
   if (r < 0)
     die("Failed to cat file %s: %s\n", argv[1], strerror(-r));
 
--- util.h
+++ util.h
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <linux/openat2.h>
@@ -11,6 +12,8 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+__attribute__((noreturn))
+__attribute__((format(printf, 1, 2)))
 static inline void die(const char *s, ...) {
   va_list ap;
   va_start(ap, s);
@@ -30,14 +33,26 @@ static inline void closep(int *fd) {
 
 #define _cleanup_free_ __attribute__((cleanup(freep)))
 #define _cleanup_close_ __attribute__((cleanup(closep)))
+#define _nonnull_ __attribute__((nonnull))
 
 static inline int cat_file_inner(int fd) {
   struct stat sb;
+  int flags;
+
+  // commit some minor crimes (sendfile doesn't work on O_APPEND files)
+  flags = fcntl(STDOUT_FILENO, F_GETFL);
+  if (flags < 0)
+    return -errno;
+  if (flags & O_APPEND)
+    if (fcntl(STDOUT_FILENO, F_SETFL, flags & ~O_APPEND) < 0)
+      return -errno;
 
   if (fstat(fd, &sb) < 0)
     return -errno;
 
-  if (sendfile(STDOUT_FILENO, fd, 0, sb.st_size) < 0)
+  assert(sb.st_size >= 0);
+
+  if (sendfile(STDOUT_FILENO, fd, NULL, (size_t) sb.st_size) < 0)
     return -errno;
 
   return 0;
@@ -57,13 +72,12 @@ static inline int cat_file_fd(int path_fd) {
   _cleanup_close_ int fd = -EBADF;
   _cleanup_free_ char *proc_fd_path = NULL;
   int r;
-  struct stat sb;
 
   r = asprintf(&proc_fd_path, "/proc/self/fd/%d", path_fd);
   if (r < 0)
     return -ENOMEM;
 
-  fd = open(proc_fd_path, O_RDONLY|O_CLOEXEC);
+  fd = open(proc_fd_path, O_RDONLY);
   if (fd < 0)
     return -errno;
 
@@ -72,6 +86,6 @@ static inline int cat_file_fd(int path_fd) {
 
 /* glibc doesn't do it so I'll do it myself 😤 */
 static inline long openat2(int dirfd, const char *pathname, struct open_how *how, size_t size) {
-  return syscall(dirfd, pathname, how, size);
+  return syscall(SYS_openat2, dirfd, pathname, how, size);
 }
 
