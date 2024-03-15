4a22a5da1075e3b51d55b28b79c8b8d8eef35f3f
tritoke <tritoke@protonmail.com>
2024-03-15 17:05:22 +0000

    ✨ intial ASAN stuff

---
+++ ASAN/uaf.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int use_after_free(void) {
+  int *x = malloc(10 * sizeof(int));
+  free(x);
+  return x[4];
+}
+
+int main(int argc, char *argv[]) {
+  printf("%d\n", use_after_free());
+  
+  return EXIT_SUCCESS;
+}
